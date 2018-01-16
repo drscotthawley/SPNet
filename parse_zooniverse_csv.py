21ab06cbd882e91b848ed318cef4e747d5e9d5e2
Scott Hawley <scott.hawley@belmont.edu>
2018-01-16 15:22:28 -0600

    switched from output format from .txt to .csv

--- parse_zooniverse_csv.py
+++ parse_zooniverse_csv.py
@@ -23,20 +23,20 @@ def make_sure_path_exists(path):
             raise
 
 
-def create_text_file(filename, datalist):
+def create_meta_file(filename, datalist):
     if (debug > 2):
-        print("   Creating text file ",filename,":")
-    datastring = ""
+        print("   Creating metadata file ",filename,":")
+    datastring = "cx,cy,a,b,angle,rings\n"
     num_an = len(datalist)           # number of antinodes
     for i in range(num_an):
         if (debug > 3):
             print(" i = ",i,", an = ",an," datalist[i] = ",datalist[i])
-        datastring += "[{0}, {1}, {2}, {3}, {4}, {5}]".format(datalist[i][0], datalist[i][1], datalist[i][2], datalist[i][3], datalist[i][4], datalist[i][5])
+        datastring += "{0},{1},{2},{3},{4},{5}".format(datalist[i][0], datalist[i][1], datalist[i][2], datalist[i][3], datalist[i][4], datalist[i][5])
         if (i < num_an-1):
             datastring += "\n"
 
-    with open(filename, "w") as text_file:
-        text_file.write(datastring)
+    with open(filename, "w") as meta_file:
+        meta_file.write(datastring)
     return
 
 
@@ -61,11 +61,11 @@ for index, row in df.iterrows():
     #print(" user_name =[",user_name,"]",sep="")
     # TODO: for now we append the username to files we create; in the future it would be nice to "merge" or "combine" multiple users' work on the same image
     image_filename = os.path.splitext(ref_filename)[0]+"_"+user_name+'.png'
-    txt_filename = os.path.splitext(ref_filename)[0]+"_"+user_name+'.txt'
+    meta_filename = os.path.splitext(ref_filename)[0]+"_"+user_name+'.csv'
     jlist = json.loads(line)
     if (debug>0):
         print("sd_dict = ",sd_dict)
-        print(Fore.MAGENTA+"image_filename =",image_filename,", txt_filename = ",txt_filename)
+        print(Fore.MAGENTA+"image_filename =",image_filename,", meta_filename = ",meta_filename)
         print(Fore.GREEN+"jlist=",jlist)
     if (len(jlist) < 2):
         print(Fore.RED+"   No antinodes in this image")
@@ -98,10 +98,10 @@ for index, row in df.iterrows():
                 print(Fore.RED+"   Problem.  Ring count unusable.  Ignoring whole image")
                 problem = True
     if (False == problem):
-        txt_filename = outpath+'/'+txt_filename
+        meta_filename = outpath+'/'+meta_filename
         if (debug>0):
-            print(Fore.MAGENTA+"Output: Text file",txt_filename,": datalist = ",datalist)
-        create_text_file(txt_filename,datalist)
+            print(Fore.MAGENTA+"Output: meta file",meta_filename,": datalist = ",datalist)
+        create_meta_file(meta_filename,datalist)
         copy2(inpath+'/'+ref_filename, outpath+'/'+image_filename)
     if (debug > 0):
         print("")
