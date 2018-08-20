e3c3d8ee83f9cb1fed9cc5ad037bf1f900f8f8c9
Scott Hawley <scott.hawley@belmont.edu>
2018-08-19 22:32:49 -0500

    Working. Needed to de-normalize before plotting

--- test_spnet.py
+++ test_spnet.py
@@ -10,8 +10,9 @@ import time
 
 import keras
 import tensorflow as tf
-from tensorflow.contrib.keras.python.keras import backend as K
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, TensorBoard
+import keras.backend as K
+from keras.callbacks import Callback
+from keras.models import load_model
 import time
 from distutils.version import LooseVersion
 from operator import itemgetter
@@ -20,40 +21,33 @@ import io
 from models import *
 from utils import *
 
-# Define some colors: openCV uses BGR instead of RGB
-blue = (255,0,0)
-red = (0,0,255)
-green = (0,255,0)
-white = (255)
-black = (0)
-grey = (128)
 
 
-
-
-
-def test_network(weights_file="weights.hdf5", datapath="Train/", fraction=1.0, log_dir="logs/"):
+def test_network(weights_file="", datapath="", fraction=1.0, log_dir="", batch_size=32):
     np.random.seed(1)
 
-    # Get the data
     print("Getting data..., fraction = ",fraction)
     testpath="Test/"
-    X_test, Y_test, img_dims, test_file_list  = build_dataset(path=testpath, load_frac=fraction)
+    X_test, Y_test, test_file_list, pred_shape  = build_dataset(path=testpath, \
+        load_frac=fraction, set_means_ranges=True, batch_size=batch_size, shuffle=False)
 
-    # Instantiate the model
-    print("Instantiating model...")
-    model = setup_model(X_test, Y_test, no_cp_fatal=True, weights_file=weights_file)
+
+    print("Loading model from",weights_file)
+    model, serial_model = setup_model(X_test, try_checkpoint=True, no_cp_fatal=True, \
+        weights_file=weights_file, parallel=False)
+
 
     m = Y_test.shape[0]
-
     print("    Predicting... (m = ",m," frames in Test set)",sep="")
     start_time = time.time()
-    Y_pred = model.predict(X_test)
+    Y_pred = model.predict(X_test, batch_size=batch_size)
     elapsed = time.time() - start_time
     print("    ...elapsed time to predict = ",elapsed,"s.   FPS = ",m*1.0/elapsed)
 
     print("    Drawing sample ellipse images...")
-    show_pred_ellipses(Y_test, Y_pred, test_file_list, log_dir=log_dir)
+    make_sure_path_exists(log_dir)
+    Yt, Yp = denorm_Y(Y_test), denorm_Y(Y_pred)
+    show_pred_ellipses(Yt, Yp, test_file_list, log_dir=log_dir)
 
     return model
 
@@ -65,12 +59,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="tests network on test dataset")
     parser.add_argument('-w', '--weights', #nargs=1, type=argparse.FileType('r'),
         help='weights file in hdf5 format', default="weights.hdf5")
-    parser.add_argument('-c', '--datapath', #type=argparse.string,
+    parser.add_argument('-d', '--datapath', #type=argparse.string,
         help='Test dataset directory with list of classes', default="Test/")
     parser.add_argument('-f', '--fraction', type=float,
         help='Fraction of dataset to use', default=1.0)
     parser.add_argument('-l', '--logdir',
-            help='Directory of log files', default='logs/')
+            help='Directory of log files', default='logs/Testing/')
+    parser.add_argument('-b', '--batch_size', type=int, help='Batch size to use', default=32)
 
     args = parser.parse_args()
-    model = test_network(weights_file=args.weights, datapath=args.datapath, fraction=args.fraction, log_dir=args.logdir)
+    model = test_network(weights_file=args.weights, datapath=args.datapath, fraction=args.fraction, log_dir=args.logdir, batch_size=args.batch_size)
