936e7e35fbc2d2d9dffd7c6025492a0b88685701
Scott Hawley <scott.hawley@belmont.edu>
2020-02-25 20:43:57 -0600

    MILESTONE: 95.5% acc on 50k fake data

--- train_spnet.py
+++ train_spnet.py
@@ -11,6 +11,7 @@ from distutils.version import LooseVersion
 import PIL
 from spnet import models, utils, multi_gpu, diagnostics, callbacks
 import random
+import spnet.config as cf
 
 
 def train_network(weights_file="weights.hdf5", datapath=".", fraction=1.0, batch_size=32, \
@@ -23,7 +24,7 @@ def train_network(weights_file="weights.hdf5", datapath=".", fraction=1.0, batch
     print("pred_grid = ",pred_grid)
 
     # options for how we run the model
-    model_type = 'bigger'#'monolithic' or 'complex'
+    model_type = cf.model_type
     if model_type == 'simple':
         grayscale = False
         force_dim = 224
@@ -38,22 +39,22 @@ def train_network(weights_file="weights.hdf5", datapath=".", fraction=1.0, batch
         load_frac=fraction, set_means_ranges=True, batch_size=batch_size, pred_grid=pred_grid, \
         grayscale=grayscale, force_dim=force_dim)
 
-    X_val, Y_val, val_file_list, pred_shape  = utils.build_dataset(path=valpath, load_frac=fraction, \
+    X_val, Y_val, val_file_list, pred_shape  = utils.build_dataset(path=valpath, load_frac=1.0, \
         set_means_ranges=False, batch_size=batch_size, pred_grid=pred_grid,\
         grayscale=grayscale, force_dim=force_dim)
 
-    print("Seting up NN model...")
-    parallel=True
-    freeze_fac=0.0
+    print("Seting up NN model.  model_type = ",cf.model_type)
+    parallel=False
+    freeze_fac=0.7
     model, serial_model = models.setup_model(X_train, Y_train[0].size, no_cp_fatal=False, weights_file=weights_file, parallel=parallel, \
-        freeze_fac=freeze_fac, model_type=model_type)
+        freeze_fac=freeze_fac)
 
     # Set up callbacks
     now = time.strftime("%c").replace('  ','_').replace(' ','_')   # date, with no double spaces or spaces
     log_dir='./logs/'+now
     myprogress = callbacks.MyProgressCallback(X_val=X_val, Y_val=Y_val, val_file_list=val_file_list, log_dir=log_dir, pred_shape=pred_shape)
     checkpointer = callbacks.ParallelCheckpointCallback(serial_model, filepath=weights_file, save_every=10)
-    lr_sched = callbacks.OneCycleScheduler(lr_max=5e-4, n_data_points=X_train.shape[0], epochs=epochs, batch_size=batch_size, verbose=1)
+    lr_sched = callbacks.OneCycleScheduler(lr_max=1e-4, n_data_points=X_train.shape[0], epochs=epochs, batch_size=batch_size, verbose=1)
     #tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=False, write_images=False)  #Tensorfboard can be a memory hog.
     #earlystopping = EarlyStopping(patience=5)
     callback_list = [myprogress, checkpointer, lr_sched]# , earlystopping]#, tensorboard]
@@ -62,7 +63,7 @@ def train_network(weights_file="weights.hdf5", datapath=".", fraction=1.0, batch
         aug_on_fly = callbacks.AugmentOnTheFly(X_train, Y_train, aug_every=2)
         callback_list.append(aug_on_fly)
 
-    frozen_epochs = 0;  # how many epochs to first run with last layers of model frozen
+    frozen_epochs = 2;  # how many epochs to first run with last layers of model frozen
     later_epochs = epochs - frozen_epochs
 
     # early training with partially-frozen pre-trained model
@@ -70,7 +71,7 @@ def train_network(weights_file="weights.hdf5", datapath=".", fraction=1.0, batch
         model.fit(X_train, Y_train, batch_size=batch_size, epochs=frozen_epochs, shuffle=True,
                   verbose=1, validation_data=(X_val, Y_val), callbacks=callback_list)
     if (freeze_fac > 0.0):
-        model = unfreeze_model(model, X_train, Y_train, parallel=parallel)
+        model = models.unfreeze_model(model, X_train, Y_train, parallel=parallel)
 
     # main training block
     model.fit(X_train, Y_train, batch_size=batch_size, epochs=later_epochs, shuffle=True,
@@ -92,7 +93,7 @@ if __name__ == '__main__':
      # greater batch size runs faster but may yield Out Of Memory errors
      # also note that small batches may yield better generalization: https://arxiv.org/pdf/1609.04836.pdf
     parser.add_argument('-b', '--batch_size', type=int, help='Batch size to use', default=36)
-    parser.add_argument('-d', '--datapath', help='Train dataset directory with list of classes', default="./")
+    parser.add_argument('-d', '--datapath', help='Directory with images in Train/ and Val/ subdirs', default="./")
     parser.add_argument('-e', '--epochs', type=int, help='Number of epochs to run', default=30)
     parser.add_argument('-f', '--fraction', type=float, help='Fraction of dataset to use (for quick testing: -f 0.05)', default=1.0)
     parser.add_argument('-g', '--grid', help='Shape of predictor grid', default="6x6x2")
