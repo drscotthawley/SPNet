bd8092e49f9d5ae677626fe924c32e4b4a0291c8
Scott Hawley <scott.hawley@belmont.edu>
2020-06-20 21:24:00 -0500

    updated diagnostic/iou test call

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -1,13 +1,18 @@
+#! /usr/bin/env python
+
 import numpy as np
 import sys
-sys.path.append("..")
-import evaluate_spnet
+sys.path.append("spnet/")
+sys.path.append("../spnet/")
+from diagnostics import compute_iou
 
 def test_compute_iou():
-    Y_true = (100,  140, 120,  60,     90, 0, 10.3)
-    Y_pred = (120,  123, 120,  60, 149.97, 0,  7.8)
-    iou = evaluate_spnet.compute_iou(Y_true, Y_pred)
-    np.testing.assert_equal(iou, 0.44227983107795693)
+    # make up two ellipes
+    Y_true = (100,  140, 120,  60,     90, 0, 10.3)  # ellipse a
+    Y_pred = (120,  123, 120,  60, 149.97, 0,  7.8)  # ellips b
+    #iou = evaluate_spnet.compute_iou(Y_true, Y_pred)
+    iou = compute_iou(Y_true, Y_pred)
+    np.testing.assert_equal(iou, 0.44227983107795693) # force an assertion for the test
     return iou
 
 
